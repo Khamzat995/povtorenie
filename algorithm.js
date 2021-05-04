d60c3f8b11ba3744ba3dded20fc28ca4be1fe83c
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-05-04 12:07:18 +0300

    jhhhgg

--- algorithm.js
+++ algorithm.js
@@ -1,6 +1,14 @@
-const getDayName = function (day) {
-  if (typeof day !== 'number') {
+const getDayName = days => {
+  if (typeof days !== 'number') {
     console.log('err')
   }
+  if (days > 0 && days < 8 === true) {
+    let days = ['tus', 'wed', 'thr', 'frd', 'sut', 'sun', 'mon'];
+    console.log ('все верно')
+  } else {
+    console.log('не верные данные')
+
+  }
 }
-getDayName('khamzat')
+
+getDayName('tus')
